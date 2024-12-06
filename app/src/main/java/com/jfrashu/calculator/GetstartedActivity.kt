963d6897f2360d30a395058d65ea94baf1694bc8
JFRashu <u2004090@student.cuet.ac.bd>
2024-12-06 09:58:29 +0600

    Landscape view and getstarted event added

--- app/src/main/java/com/jfrashu/calculator/GetstartedActivity.kt
+++ app/src/main/java/com/jfrashu/calculator/GetstartedActivity.kt
@@ -1,6 +1,8 @@
 package com.jfrashu.calculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,13 @@ class GetstartedActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_getstarted)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val getStartedBtn = findViewById<Button>(R.id.getbtn)
+
+        getStartedBtn.setOnClickListener {
+
+
+            val intent = Intent(this@GetstartedActivity, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
